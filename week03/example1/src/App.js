aba8e069dd3802a0a9a32a5d87f5712da7d0a61a
Soohyeuk <csh040816@gmail.com>
2025-09-30 13:45:07 -0400

    week03a done

--- week03/example1/src/App.js
+++ week03/example1/src/App.js
@@ -1,15 +1,34 @@
-import './index.css';
+import Button from './components/Button'
 
-function App() {
+const App = () => {
+  //we prepped everything in button component, which is now handy to use it as a prop
   return (
-    <div className="App">
-      <Button />
-      <Button />
-      <Button />
-      <Button />
-      <Button />
-    </div>
-  );
+    <>
+      <div>
+        <Button primary danger>
+          Buy Now
+        </Button>
+      </div>
+      <div>
+        <Button secondary rounded>
+          Secondary Button
+        </Button>
+      </div>
+      <div>
+        <Button danger>Delete</Button>
+      </div>
+      <div>
+        <Button warning outline rounded>
+          Are you sure?
+        </Button>
+      </div>
+      <div>
+        <Button success outline>
+          Success
+        </Button>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
--- week03/example1/tailwind.config.js
+++ week03/example1/tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+// just have this file to use tailwindcss
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
